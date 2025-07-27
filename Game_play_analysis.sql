c45ac6b790634d05bc5ce81517277b168e3a9974
Abhinav Pathania <112753370+abhinavpathania@users.noreply.github.com>
2025-07-27 13:00:41 +0530

    Update and rename Game_play_analysis to Game_play_analysis.sql

--- Game_play_analysis.sql
+++ Game_play_analysis.sql
@@ -1,3 +1,4 @@
+'
 Table: Activity
 
 +--------------+---------+
@@ -39,6 +40,7 @@ Output:
 +-----------+
 Explanation: 
 Only the player with id 1 logged back in after the first day he had logged in so the answer is 1/3 = 0.33
+'
 
 Query:
 
